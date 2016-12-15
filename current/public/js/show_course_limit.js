bd84ff50c4cabede30394cc3e4b6900832aefcc3
ottokang <ottokang@gmail.com>
2016-12-15 10:40:41 +0800

    - 變更資料夾

--- current/public/js/show_course_limit.js
+++
@@ -1,7 +0,0 @@
-/**
- * 顯示排課限制選擇區
- */
-$("#course_time_selector").toggle($("#limit_course_time").prop("checked"));
-$("#limit_course_time").click(function() {
-	$("#course_time_selector").toggle($("#limit_course_time").prop("checked"));
-}); 
